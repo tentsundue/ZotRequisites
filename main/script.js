b372cdc2da7369ee018c48a5f8567151a8295701
tentsundue <tentsundue28@gmail.com>
2023-08-24 17:24:35 -0700

    Added styling

--- main/script.js
+++ main/script.js
@@ -10,15 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
 function fetchDataFromAPI() {
     const courseNum = document.getElementById('class-id').value;
     const department = document.getElementById('dept-select').value;
-    
-    console.log(courseNum)
     const dept = encodeURIComponent(department)
     let endpoint;
     endpoint = `https://api.peterportal.org/rest/v0/courses/${dept}${courseNum}`;
-    console.group(endpoint)
     fetch(endpoint)
         .then(response => response.json())
         .then(data => {
+          
           populateTable(data);
         })
         .catch(error => console.error('Error:', error));
@@ -69,7 +67,7 @@ function retrieveTableInfo(data) {
         console.log(professors);
         let info = {
           'Prerequisites': prereqs,
-          'Frerequisites For': prereqs_for,
+          'Prerequisites For': prereqs_for,
           'Corequisites': coreqs,
           'Last 3 Terms Offered': terms_offered,
           'Professors': professors
@@ -80,7 +78,7 @@ function retrieveTableInfo(data) {
   
 }
 function populateTable(data) {
-  const table = document.getElementById('head');
+  // const table = document.getElementById('head');
   const tbody = document.getElementById('body');
 
   // Clearing current contents in the table
@@ -90,25 +88,33 @@ function populateTable(data) {
     // Loop through each key of the info object
     Object.keys(info).forEach(key => {
       const row = document.createElement('tr');
-      
+  
       // Create a cell for the key (e.g., 'prereqs', 'coreqs', ...)
       const keyCell = document.createElement('th');
       keyCell.textContent = key;
       row.appendChild(keyCell);
-  
+
       // Create a cell for the value associated with the key
       const valueCell = document.createElement('td');
-      valueCell.textContent = info[key];
+      if (Array.isArray(info[key]) && info[key].length > 0) {
+        const list = document.createElement('ul');
+        info[key].forEach(item => {
+            const bulletpt = document.createElement('li');
+            bulletpt.textContent = item;
+            list.appendChild(bulletpt);
+        });
+        valueCell.appendChild(list);
+      } else {
+        valueCell.textContent = info[key];
+      }
+    
       console.log(info[key]);
       row.appendChild(valueCell);
-  
+
       tbody.appendChild(row);
+
     })
   )
-
-
-
-  document.getElementById("border-table").style.border = '2px solid black';
 
 }
 
